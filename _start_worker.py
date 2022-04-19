398c2bb0319c299da74e64fd2acec5f80e6750f0
Paul Leveau <plawn.yay@gmail.com>
2022-04-19 21:01:58 +0200

    add provider settings

--- _start_worker.py
+++ _start_worker.py
@@ -5,7 +5,7 @@ from services.worker.app.worker import start_worker
 
 if __name__ == '__main__':
     conf_filename = os.environ.get('CONF_FILE', 'conf.yaml')
-    template_provider = 'local'
+    template_provider = os.environ.get('TEMPLATE_PROVIDER', 'local')
     try:
         start_worker(conf_filename, template_provider)
     except KeyboardInterrupt:
