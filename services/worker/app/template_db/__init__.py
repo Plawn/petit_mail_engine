ae9ce0e204821b8caf32d14cb717def4edd46376
Paul Leveau <plawn.yay@gmail.com>
2022-11-06 22:07:08 +0100

    fix import scheme

--- services/worker/app/template_db/__init__.py
+++ services/worker/app/template_db/__init__.py
@@ -1,7 +1,6 @@
 from typing import Dict, Type
 
-from services.worker.app.template_db.git_implem import GITImplem
-
+from .git_implem import GITImplem
 from .interface import TemplateData, TemplateDB
 from .local_implem import LocalTemplateDB
 from .minio_implem import MinioInfos, MinioTemplateDB
