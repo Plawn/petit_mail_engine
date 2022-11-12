bd69f0dfbbe234e1eddc5e9a963af6dbe483f297
Paul Leveau <plawn.yay@gmail.com>
2022-11-12 11:13:38 +0100

    minor fix

--- services/worker/app/template_db/interface.py
+++ services/worker/app/template_db/interface.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod, abstractstaticmethod
 import logging
-from typing import Any, Callable, Dict, Generic, List, Optional, Tuple, TypeVar, final
+from typing import Any, Callable, Dict, Final, Generic, List, Optional, Tuple, TypeVar, final
 
 import premailer
 from jinja2 import Environment, FunctionLoader, select_autoescape
@@ -9,11 +9,24 @@ from ..render_functions import RenderFunctions
 from .utils import find_between, get_placeholder
 
 
+def make_delimiter(token: str) -> Tuple[str, str]:
+    return (f'<{token}>', f'</{token}>')
+
+
+SUBJECT_TOKEN: Final = 'subject'
+SUBJECT_TOKEN_START, SUBJECT_TOKEN_END = make_delimiter(SUBJECT_TOKEN)
+
+MAIL_CONTENT_TOKEN: Final = 'mail_content'
+MAIL_CONTENT_TOKEN_START, MAIL_CONTENT_TOKEN_END = make_delimiter(
+    MAIL_CONTENT_TOKEN)
+
+
 class TemplateData:
     def __init__(self, text: str, is_common: bool, fields: List[str]):
-        self.subject = find_between(text, '<subject>', '</subject>')[0]
+        self.subject = find_between(
+            text, SUBJECT_TOKEN_START, SUBJECT_TOKEN_END)[0]
         self.body = find_between(
-            text, '<mail_content>', '</mail_content>'
+            text, MAIL_CONTENT_TOKEN_START, MAIL_CONTENT_TOKEN_END
         )[0] if not is_common else text
         self.fields = fields
 
@@ -26,7 +39,7 @@ class TemplateDB(ABC, Generic[T]):
         self.templates: Dict[str, TemplateData] = {}
         self.logger = logger or logging.getLogger()
         self.env = Environment(
-            loader=FunctionLoader(self.__get_template),
+            loader=FunctionLoader(self._get_template),
             autoescape=select_autoescape(['html', 'xml'])
         )
         self.env.auto_reload = True
@@ -46,14 +59,14 @@ class TemplateDB(ABC, Generic[T]):
         self.env.globals.update(self.render_functions)
 
     @final
-    def __get_template(self, name: str) -> str:
+    def _get_template(self, name: str) -> str:
         if name[-9:] == '_subject!':
             return self.templates[name[:-9]].subject
         else:
             return self.templates[name].body
 
     @final
-    def add_template_from_text(self, template_name: str, text: str, is_common=False):
+    def add_template_from_text(self, template_name: str, text: str, is_common: bool = False):
         self.templates[template_name] = TemplateData(
             text, is_common, get_placeholder(
                 text, list(self.render_functions.keys()))
