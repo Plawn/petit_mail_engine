74aed3b31d5fa84e83b6b24404284dd5837229db
Plawn <plawn.yay@gmail.com>
2022-11-25 13:27:55 +0000

    fix import

--- services/db_definition/__init__.py
+++ services/db_definition/__init__.py
@@ -1,7 +1,7 @@
 from .email import Email
 from .recipient import Recipient
 from .content import Content
-from .base import database, init_db, credentials
+from .base import database, init_db, credentials, minimal_settings
 from .sender import Sender
 from .identity import Identity
 from .common_content import CommonContent
