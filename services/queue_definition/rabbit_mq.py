2f9d1bfa24c38898a184e291ef10bcecc286b409
Plawn <plawn.yay@gmail.com>
2022-12-14 12:42:41 +0000

    added monitor to ensure queue connection

--- services/queue_definition/rabbit_mq.py
+++ services/queue_definition/rabbit_mq.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
-from typing import Generic, TypeVar
-
+from typing import Generic, TypeVar, Optional
+import threading
+import logging
 import pika
-from pika.adapters.blocking_connection import BlockingChannel
+from pika.adapters.blocking_connection import BlockingChannel, BlockingConnection
 from pika.spec import Basic
 
 from .interface import CallbackType, ChannelInterface, QueueACK, QueueInterface
@@ -31,18 +32,42 @@ class RabbitACK(Generic[T], QueueACK[T]):
         self.channel.basic_nack(delivery_tag=self.method.delivery_tag)
 
 
-class RabbitChannel(ChannelInterface[T]):
-    def __init__(self, channel: BlockingChannel) -> None:
+class Monitor(threading.Thread):
+    def __init__(self, channel: BlockingConnection, logger: Optional[logging.Logger] = None):
         self.channel = channel
+        self.stopped = False
+        self.running = False
+        self.logger = logger or logging.getLogger()
+    
+    def run(self):
+        self.running = True
+        self.logger.info("Monitor started")
+        while not self.stopped:
+            self.logger.info("Monitor ensure events begin processed")
+            self.channel.process_data_events() # prevent timeout
+            time.sleep(10)
+        self.running =  False
+
+    def stop(self):
+        self.stopped = True
+        self.join()
+
+
+class RabbitChannel(ChannelInterface[T]):
+    def __init__(self, connection: RabbitMQImplem, channel: BlockingChannel) -> None:
+        self.channel = channel
+        self.connection = connection
+
 
     def add_consumer(self, name: str, consumer: CallbackType[T]) -> None:
-        def wrapped_callback(ch: BlockingChannel, method: Basic.Deliver, properties: pika.BasicProperties, body: bytes):
+        def wrapped_callback(ch: BlockingChannel, method: Basic.Deliver, properties: pika.BasicProperties, body: bytes): # TODO: body type should be generic
             consumer(body, RabbitACK(ch, method))
 
         self.channel.basic_consume(
             name, on_message_callback=wrapped_callback, auto_ack=False)
 
     def start(self):
+        self.connection._ensure_started()
         self.channel.start_consuming()
 
     def stop(self):
@@ -64,6 +89,14 @@ class RabbitMQImplem(Generic[T], QueueInterface[RabbitMQConf, T]):
                 )
             )
         )
+        self.monitor = Monitor(self.connection)
+
+    def _ensure_started(self):
+        if not self.monitor.running:
+            self.monitor.start()
+
+    def stop(self):
+        self.monitor.stop()
 
     @staticmethod
     def get_configurer():
@@ -72,4 +105,4 @@ class RabbitMQImplem(Generic[T], QueueInterface[RabbitMQConf, T]):
     def declare_queue(self, name: str, passive: bool) -> RabbitChannel[T]:
         channel = self.connection.channel()
         channel.queue_declare(queue=name, passive=passive)
-        return RabbitChannel(channel=channel)
+        return RabbitChannel(self, channel=channel)
