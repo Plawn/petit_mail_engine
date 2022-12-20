02a7c7929da28cf9034286a2a7ddc1f942cbbb83
Plawn <plawn.yay@gmail.com>
2022-12-20 10:55:19 +0000

    re add git implem

--- services/worker/app/template_db/__init__.py
+++ services/worker/app/template_db/__init__.py
@@ -1,6 +1,6 @@
-from typing import Dict, Type
+from typing import Dict, Type, Optional
+import os
 
-# from .git_implem import GITImplem
 from .interface import TemplateData, TemplateDB
 from .local_implem import LocalTemplateDB
 from .minio_implem import MinioInfos, MinioTemplateDB
@@ -8,9 +8,21 @@ from .minio_implem import MinioInfos, MinioTemplateDB
 engines: Dict[str, Type[TemplateDB]] = {
     's3': MinioTemplateDB,
     'local': LocalTemplateDB,
-    # 'git': GITImplem,
 }
 
+def is_variable_true(variable_name: str) -> Optional[bool]:
+    value = os.environ.get(variable_name)
+    if value is None:
+        return None
+    if value.lower() == "true":
+        return True
+    if value.lower() == "false":
+        return False
+    return None 
+
+if not is_variable_true("DISABLE_GIT"):
+    from .git_implem import GITImplem
+    engines["git"] = GITImplem
 
 def add_engine(name: str, engine: Type[TemplateDB]) -> None:
     engines[name] = engine
