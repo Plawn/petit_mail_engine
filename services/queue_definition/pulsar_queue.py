8da533eb647c1df2743028f4d7e293282349bcc9
Plawn <plawn.yay@gmail.com>
2022-12-22 15:37:52 +0000

    wip

--- services/queue_definition/pulsar_queue.py
+++ services/queue_definition/pulsar_queue.py
@@ -1,21 +1,30 @@
 from __future__ import annotations
 
-from pulsar import Client, MessageId
+from pulsar import Client, MessageId, Consumer, Message
+import pulsar
+from dataclasses import dataclass
+from typing import Generic, TypeVar, Optional
+import logging
+
+import time
+from .interface import CallbackType, ChannelInterface, QueueACK, QueueInterface
+
+T = TypeVar('T')
+
 
 @dataclass
 class PulsarConf:
     host: str
     port: str
 
-
+"""
 client = pulsar.Client('pulsar://localhost:6650')
 
-producer = client.create_reader("QUEUE", MessageId.earliest)
+consumer = client.subscribe(
+    topic='my-topic',
+    subscription_name='my-subscription',
+    schema=AvroSchema(Example))
 
-for i in range(10):
-    producer.send(('Hello-%d' % i).encode('utf-8'))
-
-client.close()
 
 while True:
     msg = consumer.receive()
@@ -27,20 +36,7 @@ while True:
     except Exception:
         # Message failed to be processed
         consumer.negative_acknowledge(msg)
-
-
-from dataclasses import dataclass
-from typing import Generic, TypeVar, Optional
-import threading
-import logging
-import pika
-from pika.adapters.blocking_connection import BlockingChannel, BlockingConnection
-from pika.spec import Basic
-import time
-from .interface import CallbackType, ChannelInterface, QueueACK, QueueInterface
-
-T = TypeVar('T')
-
+"""
 
 @dataclass
 class RabbitMQConf:
@@ -48,45 +44,48 @@ class RabbitMQConf:
     user: str
     password: str
 
+# OK
+class PulsarACK(Generic[T], QueueACK[T]):
 
-class RabbitACK(Generic[T], QueueACK[T]):
-
-    def __init__(self, channel: BlockingChannel, method: Basic.Deliver) -> None:
-        self.channel = channel
-        self.method = method
+    def __init__(self, consumer: Consumer, message: Message) -> None:
+        self.consumer = consumer
+        self.message = message
 
     def success(self) -> None:
-        self.channel.basic_ack(delivery_tag=self.method.delivery_tag)
+        self.consumer.acknowledge(self.message)
 
     def failed(self, reason: str) -> None:
         """reason is ignored for now"""
-        self.channel.basic_nack(delivery_tag=self.method.delivery_tag)
+        self.consumer.negative_acknowledge(message)
 
 
-class RabbitChannel(ChannelInterface[T]):
-    def __init__(self, connection: RabbitMQImplem, channel: BlockingChannel) -> None:
-        self.channel = channel
+class PulsarChannel(ChannelInterface[T]):
+    def __init__(self, connection: PulsarImplem, channel: Consumer) -> None:
         self.connection = connection
-
+        self.channel = channel
+        
     def add_consumer(self, name: str, consumer: CallbackType[T]) -> None:
         # TODO: body type should be generic
-        def wrapped_callback(ch: BlockingChannel, method: Basic.Deliver, properties: pika.BasicProperties, body: T):
-            consumer(body, RabbitACK(ch, method))
+        def wrapped_callback(ch: Consumer, method: Basic.Deliver, properties: pika.BasicProperties, body: T):
+            consumer(body, PulsarACK(ch, method))
 
         self.channel.basic_consume(
             name, on_message_callback=wrapped_callback, auto_ack=False)
 
     def start(self):
-        self.ensure_ready()
-        self.channel.start_consuming()
+        while True:
+            msg = consumer.receive()
+            ex = msg.value()
+            try:
+                print("Received message a={} b={} c={}".format(ex.a, ex.b, ex.c))
+                # Acknowledge successful processing of the message
+                consumer.acknowledge(msg)
+            except Exception:
+                # Message failed to be processed
+                consumer.negative_acknowledge(msg)
 
     def stop(self):
         self.channel.stop_consuming()
-
-    def ensure_ready(self) -> None:
-        if not self.connection.is_opened():
-            raise Exception(
-                'Not opened, call init() on the connection or open() on the channel before use')
 
     def publish(self, name: str, data: T):
         self.ensure_ready()
@@ -97,41 +96,26 @@ class RabbitChannel(ChannelInterface[T]):
         return self
 
 
-class RabbitMQImplem(Generic[T], QueueInterface[RabbitMQConf, T]):
-    def __init__(self, conf: RabbitMQConf) -> None:
+class PulsarImplem(Generic[T], QueueInterface[PulsarConf, T]):
+    def __init__(self, conf: PulsarConf) -> None:
         super().__init__(conf)
-        self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(
-                host=conf.host,
-                heartbeat=30,
-                credentials=pika.PlainCredentials(
-                    username=conf.user,
-                    password=conf.password,
-                )
-            )
-        )
-        self.monitor = Monitor(self.connection)
+        self.connection = pulsar.Client(f'pulsar://{conf.host}:{conf.port}')
 
     def init(self):
-        self._ensure_started()
         self.did_init = True
         return self
-
-    def _ensure_started(self):
-        if not self.monitor.is_running():
-            self.monitor.start()
 
     def is_opened(self):
         return self.did_init
 
     def stop(self):
-        self.monitor.stop()
+        pass
 
     @staticmethod
     def get_configurer():
-        return RabbitMQConf
+        return PulsarConf
 
     def declare_queue(self, name: str, passive: bool) -> RabbitChannel[T]:
         channel = self.connection.channel()
         channel.queue_declare(queue=name, passive=passive)
-        return RabbitChannel(self, channel=channel)
+        return PulsarChannel(self, channel=channel)
